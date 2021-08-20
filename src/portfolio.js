2166b797038cd64c322448fbe52435a7e24ae024
Alaa OMRI <omri.a@decade.fr>
2021-08-20 19:22:12 +0100

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,174 @@
+import emoji from "react-easy-emoji";
+
+import decade from "./assets/img/icons/common/dkd.png";
+
+export const greetings = {
+  brand: "Alaa",
+  name: "Alaa OMRI",
+  title: "Hi all, I'm Alaa Eddine OMRI",
+  description:
+    "A passionate Full Stack Web Developer having an experience of building e-commerce Web applications with JavaScript / Reactjs / Redux / Nextjs / Java / JEE / Hybris / .Net and some other cool libraries and frameworks.",
+  resumeLink:
+    "https://drive.google.com/file/d/1hYpKIEmG8vXc23HygPKF9-mn1b5l6QD1/view?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "alaaomri",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/alaaomri.54474",
+  instagram: "https://www.instagram.com/alaa__omri/",
+  twitter: "https://twitter.com/omrialaaeddine",
+  github: "https://github.com/alaaomri",
+  linkedin: "https://tn.linkedin.com/in/alaa-omri-6b6046141/",
+  phone: "tel:+21621009473",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle:
+    "PASSIONATE FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [emoji("⚡ "), emoji("⚡ "), emoji("⚡ ")],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "redux",
+      fontAwesomeClassname: "logos:redux",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "Nextjs",
+      fontAwesomeClassname: "logos:nextjs",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "logos:java",
+    },
+    {
+      skillName: "Csharp",
+      fontAwesomeClassname: "vscode-icons:file-type-csharp",
+    },
+    {
+      skillName: "NET",
+      fontAwesomeClassname: "logos:dotnet",
+    },
+    {
+      skillName: "JEE",
+      fontAwesomeClassname: "vscode-icons:file-type-jsp",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "REST",
+      fontAwesomeClassname: "vscode-icons:file-type-rest",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "vscode-icons:file-type-git",
+    },
+    {
+      skillName: "jira",
+      fontAwesomeClassname: "logos:jira",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "70", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "80",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "80",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Faculty of sciences of Tunisia",
+    subHeader: "Master of Science in Computer Science",
+    duration: "September 2016 - April 2019",
+    desc: "National Diploma in Computer Science Engineering",
+    descBullets: [],
+  },
+  {
+    schoolName: "Harvard",
+    subHeader: "Master of Science in Computer Science",
+    duration: "September 2017 - April 2019",
+    desc: "Participated in the research of XXX and published 3 papers.",
+    descBullets: [],
+  },
+];
+
+export const experience = [
+  {
+    role: "Software Engineer",
+    company: "DECADE",
+    companylogo: decade,
+    date: "April 2021 – Present",
+    desc: "Participating in developping e-commerce B2C website using Reactjs/ Redux/ Nextjs / .Net",
+    descBullets: [],
+  },
+  {
+    role: "Software Engineer",
+    company: "DECADE",
+    companylogo: decade,
+    date: "July 2019 – Mars 2021",
+    desc: "Participating in developping e-commerce B2C website using Reactjs/ Java JEE/ Spring MVC / SAP Hybris",
+  },
+  {
+    role: "Software Engineer Intern",
+    company: "DECADE",
+    companylogo: decade,
+    date: "February 2019 – Mai 2019",
+    desc: "Integration of Algolia search Engine in an e-commerce website engined by hybris platform",
+  },
+];
+
+export const projects = [];
+export const feedbacks = [];
