6aca41f70edc579ee20ee0a4a294f5595ca1088f
Alaa OMRI <omri.a@decade.fr>
2021-09-07 19:30:05 +0100

    datalayer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import 'assets/vendor/nucleo/css/nucleo.css';
+import 'assets/vendor/font-awesome/css/font-awesome.min.css';
+import 'assets/css/argon-design-system-react.css';
+import 'i18n';
 
-import Navigation from "./components/Navigation";
-import Greetings from "./containers/Greetings";
-import Skills from "./containers/Skills";
-import Proficiency from "./containers/Proficiency";
-import Education from "./containers/Education";
-import Experience from "./containers/Experience";
-import Projects from "./containers/Projects";
-import GithubProfile from "./containers/GithubProfile";
-import Feedbacks from "./containers/Feedbacks";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/font-awesome/css/font-awesome.min.css";
-import "assets/css/argon-design-system-react.css";
-import "i18n";
-ReactDOM.render(
-  <>
-    <Navigation />
-    <Greetings />
-    <Skills />
-    <Proficiency />
-    <Education />
-    <Experience />
-    <Feedbacks />
-    <Projects />
-    <GithubProfile />
-  </>,
-  document.getElementById("root")
-);
+import App from 'App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
